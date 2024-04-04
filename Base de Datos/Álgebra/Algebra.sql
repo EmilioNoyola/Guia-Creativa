6ce8207832874e8c35a7f82eda1d93984d6a95a3
Emilio <noyola.gomez.emilio.damian@gmail.com>
2024-04-04 00:47:23 -0600

    Agregue imagenes a la base de datos de algebra

--- Base de Datos/Álgebra/Algebra.sql
+++ Base de Datos/Álgebra/Algebra.sql
@@ -39,7 +39,7 @@ CREATE TABLE PreguntasA(
 --Preguntas Primer Parcial Álgebra.
 
 --Tema 1 Operaciones con Fracciones
-INSERT INTO PreguntasA VALUES (111,01,'aqui va a haber una imagen');
+INSERT INTO PreguntasA VALUES (111,01,'https://github.com/EmilioNoyola/Guia-Creativa/blob/main/Base%20de%20Datos/%C3%81lgebra/111-01.png?raw=true');
 INSERT INTO PreguntasA VALUES (111,02,'aqui va a ir otra imagen a) b) c)');
 INSERT INTO PreguntasA VALUES (111,03,' a) b) c)');
 INSERT INTO PreguntasA VALUES (111,04,' a) b) c)');
