8f35ae85a79d9db9056ceee7432f4722e7f5a19a
Xiwei <huangg.xiwei@gmail.com>
2024-04-07 15:21:00 -0500

    Modifique bases de datos F1

--- Base-de-Datos/Fisica-1/DB_Guia_Creativa_FI.sql
+++ Base-de-Datos/Fisica-1/DB_Guia_Creativa_FI.sql
@@ -166,5 +166,135 @@ C) Tienen una magnitud de 1 y su dirección puede variar.
 D) No tienen magnitud ni dirección definida.
 ");
 
-INSERT INTO PreguntasF1 VALUES(141,09,"	Es Factible la siguiente operación ( a∙b )+ c ? (Si o no) fundamenta tu respuesta
+INSERT INTO PreguntasF1 VALUES(141,09,"	Es Factible la siguiente operación ( A∙B )+ C (Si o no) fundamenta tu respuesta
+A) Sí, porque el resultado de la multiplicación punto entre dos vectores es otro vector, por lo que es posible sumarlo con otro vector.
+--B) No, porque el resultado de multiplicar dos vectores mediante el producto punto te da un escalar, entonces estarías sumando un escalar más un vector.
+C) No, porque el resultado de la multiplicación punto entre dos vectores es un vector, por lo que no se puede sumar con otro vector.
+D) Sí, porque el resultado de la multiplicación punto entre dos vectores es un vector, y sumar otro vector a este resultado es una operación válida en el álgebra lineal.
+");
+
+INSERT INTO PreguntasF1 VALUES(141,10," Para el vector A=6i-18j+8k  Los Ángulos directores son:
+--A) 0°, 270°, 360°
+B) 0°, 90°, 180°
+C)90°, 180°, 270°
+D)0°, 180°, 270°
+");
+
+INSERT INTO PreguntasF1 VALUES(141,11,"Escribe el enunciado del principio de transmisibilidad de las fuerzas.
+A) La magnitud de una fuerza puede cambiar si su dirección se modifica.
+B) El punto de aplicación de una fuerza debe permanecer fijo para mantener su efecto.
+--C) El punto de aplicación de la fuerza puede moverse a cualquier parte a lo largo de su línea de acción.
+D) Las fuerzas pueden transmitirse a través de medios distintos del contacto directo.
+");
+
+INSERT INTO PreguntasF1 VALUES(141,12,"Dos vectores F1 y F2 tiene las siguientes componentes F1x=6N,F1y= -5N y F2x= -6N
+A) 0.5 A
+B) A
+C) 2 A
+--D) 0.866 A
+");
+INSERT INTO PreguntasF1 VALUES(141,13,"¿Cuál es el concepto de producto escalar?
+A) El producto cruz de dos vectores.
+B) La suma de las componentes de dos vectores.
+C) El resultado de multiplicar dos vectores componente a componente.
+--D) El producto punto de los vectores A y B, que se escribe A·B y se lee “A punto B”, se define como el producto de las magnitudes de A y B y el coseno del ángulo θ entre sus colas.
+");
+
+INSERT INTO PreguntasF1 VALUES(141,14,"La Magnitud y Direccion del vector  A= -3.4 i+3.4 j
+--A) 4.808, θ= 315°
+B) 3.4, θ= 225°
+C) 4.808, θ= 135°
+D) 3.4, θ= 45°
+");
+
+--Tema 2 primer parcial
+
+INSERT INTO PreguntasF1 VALUES(142,15," ¿Qué es medir?
+--A) Es la acción de comparar un objeto o cosa con un patrón de medida previamente establecido y validado.
+
+B) Es la acción de observar un objeto sin compararlo con ningún patrón.
+
+C) Es la acción de crear un patrón de medida.
+
+D) Es la acción de pesar un objeto.
+");
+
+INSERT INTO PreguntasF1 VALUES(142,16,"Explica qué es la resolución de un instrumento de medición.
+--A) Es la mínima medida que puede ser realizada con un instrumento de medición.
+
+B) Es la máxima medida que puede ser realizada con un instrumento de medición.
+
+C) Es la precisión de un instrumento de medición.
+
+D) Es la exactitud de un instrumento de medición.
+");
+
+INSERT INTO PreguntasF1 VALUES(142,17,"Elige 3 de las cantidades fundamentales del sistema internacional de unidades.
+--A) Amperio, Kelvin, mol
+
+B) Metro, Segundo, Libra
+
+C) Litro, Hora, Newton
+
+D) Voltio, Joule, Pascal
+");
+
+INSERT INTO PreguntasF1 VALUES(142,18,"¿Cuál es la diferencia entre la medición y el valor promedio?
+A) Exactitud
+
+--B) Error absoluto
+
+C) Precisión
+
+D) Incertidumbre
+");
+
+INSERT INTO PreguntasF1 VALUES(142,19,"¿Cuál es el nombre del prefijo para expresar 10^9?
+ --A) Giga 
+ B) Tera 
+ C) Mega 
+ D) Kilo
+ ");
+
+
+INSERT INTO PreguntasF1 VALUES(142,20,"¿Cuáles son las dimensiones de la velocidad? A) M^0 L^1 T^-1 B) M^1 L^1 T^-1 C) M^1 L^0 T^-1 D) M^0 L^0 T^-1");
+
+INSERT INTO PreguntasF1 VALUES(142,21,"Un terreno rectangular tiene 100.0 ft El Área del terreno en m^2 es:
+A) 30.48m^2
+
+B) 929.0m^2
+
+C) 100.0m^2
+
+--D) 1394.0m^2
+");
+
+INSERT INTO PreguntasF1 VALUES(142,22,"¿Qué es el valor promedio de todos los datos?
+A) Mediana
+
+--B) Media
+
+C) Moda
+
+D) Rango
+");
+
+INSERT INTO PreguntasF1 VALUES(142,23,"¿Cuánto equivale un metro en pies (ft)?
+A) 0.621mi
+
+B) 1.094yd
+
+--C) 3.281ft
+
+D) 39.37in
+");
+
+INSERT INTO PreguntasF1 VALUES(142,24,"Sobre la luna, la aceleración debido a la gravedad es solo 1/6 de la que existe en la Tierra. Un astronauta cuya masa en la tierra es de 600kg se desplaza a la superficie lunar. ¿Cuál es su masa medida en la luna?
+A) 360kg
+
+B) 100kg
+
+C) 3600kg
+
+--D) 600kg
 ");
